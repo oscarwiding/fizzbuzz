7e8c5579224f5532e9640fdc53f0913513251e0c
Oscar Widing <widing.oscar@gmail.com>
2020-09-15 12:02:42 +0200

    Added an if for the hitchhiker

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 def main():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
+        if i == 42:
+            print("Answer to the Ultimate Question of Life, the Universe, and Everything")
+        elif i % 3 == 0 and i % 5 == 0:
             print("Fizzbuzz")
         elif i % 3 == 0:
             print("Fizz")
